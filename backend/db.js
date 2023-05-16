0b8a3b202e83dc967ce1eaadf5a1e299bdd58e8f
Ravi9698 <111142533+Ravi9698@users.noreply.github.com>
2023-05-16 11:27:55 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,4 @@
 const mongoose = require('mongoose');
-const mongoURI= 'mongodb+srv://ravi7755yadav:Mongo123dbgo@cluster0.eifwa5t.mongodb.net/gofoodmern?retryWrites=true&w=majority';
-//const mongoURI= 'mongodb://ravi7755yadav:Mongo123dbgo@ac-1zbfnk9-shard-00-00.eifwa5t.mongodb.net:27017,ac-1zbfnk9-shard-00-01.eifwa5t.mongodb.net:27017,ac-1zbfnk9-shard-00-02.eifwa5t.mongodb.net:27017/gofoodmern?ssl=true&replicaSet=atlas-10mmjk-shard-0&authSource=admin&retryWrites=true&w=majority';
 const mongoDB=async()=>{
             await mongoose.connect(mongoURI,{useNewUrlParser:true},async(err,result)=>{
                 if (err) console.log("---",err)
